4132f9847de8b0a07f41b0a55a3653b3e3fc019e
AbiramiR-27 <abiramesh2023@gmail.com>
2026-09-15 12:11:24 +0530

    feat: add unique employee ID problem solution for day 2

--- problems_to_be_solved_in_home/day_02/unique_employee_id.py
+++ problems_to_be_solved_in_home/day_02/unique_employee_id.py
@@ -1,3 +1,17 @@
 """
 Unique employee ID - "Yes" or "No"
 """
+
+n = int(input())
+
+for _ in range(n):
+    num = list(map(int, input().split()))
+    arr = []
+    
+    for i in num:
+        if i in arr:
+            print("No")
+            break
+        arr.append(i)
+    else:
+        print("Yes")
