de29083f1388fa02d5137428fb8f3e0b09579934
AbiramiR-27 <abiramesh2023@gmail.com>
2026-09-15 13:56:29 +0530

    feat: add day 1 DSA solutions for array min/max, odd/even averages, and largest elements

--- problems_solved_in_class/day_01/average_of_odd_and_even_numbers.py
+++ problems_solved_in_class/day_01/average_of_odd_and_even_numbers.py
@@ -5,3 +5,20 @@ Different levels of difficulties:
 - Input Validation
 - Output round off to 2 decimal places
 """
+
+arr=[1,2,3,4,5,6,7,8,9,10]
+sum_even=0
+sum_odd=0
+count_even=0
+count_odd=0
+for i in range(len(arr)):
+    if arr[i]%2==0:
+        sum_even+=arr[i]
+        count_even+=1
+    else:
+        sum_odd+=arr[i]
+        count_odd+=1
+avg_even=sum_even/count_even
+avg_odd=sum_odd/count_odd
+print("Average of even numbers:",avg_even)
+print("Average of odd numbers:",avg_odd)
